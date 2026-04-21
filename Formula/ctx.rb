c521046810499e90e9ed882b38d159818ca1cf13
Anka <runner@sat12-bq160-f55ec6eb-0a20-4047-b811-2ce7ab9e0f82-5E0EC15DB414.local>
2026-04-21 16:39:26 +0000

    ctx v0.2.1

--- Formula/ctx.rb
+++ Formula/ctx.rb
@@ -1,9 +1,9 @@
 class Ctx < Formula
   desc "Local-first memory ingest and retrieval CLI"
   homepage "https://github.com/bhimeshagrawal/ctx"
-  url "https://github.com/bhimeshagrawal/ctx/releases/download/v0.2.0/ctx-darwin-arm64.tar.gz"
-  version "0.2.0"
-  sha256 "61948f43666d7ea600a6df290b96129d63f6bc7a0e2d4f4c149922cca385b0da"
+  url "https://github.com/bhimeshagrawal/ctx/releases/download/v0.2.1/ctx-darwin-arm64.tar.gz"
+  version "0.2.1"
+  sha256 "f7b57423f193716defb12d7a49aa19f2839e8d65ceff0fb79efc762c94aefd1d"
 
   def install
     bin.install "ctx"
