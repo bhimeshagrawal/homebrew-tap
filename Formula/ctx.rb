372acf31345802e34fb5576f17e940b81a3f189b
Anka <runner@sat12-bq161-1392677e-dc27-45af-9f17-0a60a6d62a17-0247A8ECE936.local>
2026-04-21 09:47:09 +0000

    ctx v0.1.1

--- Formula/ctx.rb
+++ Formula/ctx.rb
@@ -3,7 +3,7 @@ class Ctx < Formula
   homepage "https://github.com/bhimeshagrawal/ctx"
   url "https://github.com/bhimeshagrawal/ctx/releases/download/v0.1.1/ctx-darwin-arm64.tar.gz"
   version "0.1.1"
-  sha256 "b04550c122bf8144312b74b6387fb6933e2590bb862dd179f4bd29eb71c04673"
+  sha256 "bc082d0cb4c6e9fae6212730b54a55900936104105b7c8749a0d125a7849a8ea"
 
   def install
     bin.install "ctx"
